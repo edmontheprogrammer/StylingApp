56e04782359157a153f02cf5c16e0bbcaf91fdc4
Edmon Sebit <edmontheprogrammer@gmail.com>
2023-05-28 05:41:04 +0200

    Styling App

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1, 
+      /*
+        Note 1: Anything that we put in the screen appears in the center, middle of the screen
+        because of these two properties "justifyContent" and "alignItems"
+
+        Note 2: Do not create inline styles in production, it's better to use the StyleSheet API
+        or StyleSheet components for production. 
+
+        Note 3: 
+      */
+      justifyContent: "center", 
+      alignItems: "center", 
+    }}>
+        <View  style={{
+          backgroundColor: "dodgerblue", 
+          width: 100, 
+          height: 100, 
+          borderWidth: 10, 
+          borderColor: "royalblue", 
+        }}></View>
+      
     </View>
   );
 }
